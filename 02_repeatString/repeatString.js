bac090f8c8b4a61264675dfbf4b45d95ece38bdf
Osius Kara <ouus2k@web.de>
2022-10-04 16:59:34 -0500

    completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeat = "";
 
+  while (num > 0) {
+    repeat += string;
+    num--;
+  }
+
+  if (num < 0) {
+    return "ERROR";
+  }
+  return repeat;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
