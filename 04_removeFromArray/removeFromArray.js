2ab3263bd28281846801e6651c9d4132fe036cea
Osius Kara <ouus2k@web.de>
2022-10-07 12:02:06 -0500

    completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  const array = args[0]; // [1, 2, 3, 4]
+  const newArray = []; //  becomes [1,2,4] after the forEach loop
 
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
+
 module.exports = removeFromArray;
