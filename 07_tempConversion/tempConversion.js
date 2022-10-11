cd51d31b6ea68475354f350c27982a6e083639e6
Osius Kara <ouus2k@web.de>
2022-10-11 14:36:18 -0500

    exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function (fahr) {
+  const celsius = Math.round((fahr - 32) * (5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
+ftoc();
 
+const ctof = function (celsius) {
+  const fahrenheit = Math.round((celsius * (9 / 5) + 32) * 10) / 10;
+  return fahrenheit;
 };
+
+ctof();
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
